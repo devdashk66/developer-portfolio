184e2f612e38d76b49e2785c495bcba6690e1d16
raju-miah <rajumiahpollob7@gmail.com>
2022-12-12 12:30:04 +0600

    link

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -10,6 +10,10 @@ const Work = () => {
             </h2>
             <span className='section-subtitle'>My Recent Work</span>
 
+
+            {/* https://preview.themeforest.net/item/luique-one-page-resume-wordpress-theme/full_screen_preview/41227879?_ga=2.261783565.1061629833.1670450356-195235480.1669739008 */}
+
+
             <Works></Works>
         </section>
     );
